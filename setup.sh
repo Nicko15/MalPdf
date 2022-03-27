4209ec9964c2d5b9669b1e982092b5a08b341a90
Nicko15 <84836228+Nicko15@users.noreply.github.com>
2022-03-27 11:43:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+apt-get upgrade
+apt-get update
+apt upgrade
+apt update
+
+apt install curl
+apt install chmod
+apt install sudo
+apt install bash
+apt install git
+
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
   sudo bash ./msfinstall
